3fe540f42d7f7f28116411feecea3854ae449de0
matt-chinchilla <chirichella.matt@gmail.com>
2024-04-18 16:12:47 -0400

    I changed things

--- Recursion/BinaryTreeTraversal/BinaryTreeVisualization.java
+++ Recursion/BinaryTreeTraversal/BinaryTreeVisualization.java
@@ -18,6 +18,8 @@ public class BinaryTreeVisualization extends Application{
         tree.root.left.right = new Node(5);
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
+        tree.root.left.left.left = new Node(8);
+        tree.root.left.left.right = new Node(9);
 
 
     //Draw the tree, make the class later
